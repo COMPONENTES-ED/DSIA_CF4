b41258fd80f9c60aacf64911edc476c049adbd4c
yesnapoleondynamite <yesnapoleondynamite@gmail.com>
2025-06-16 07:36:45 -0500

    Cargue_inicial

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# DSIA_CF4
+# **Ajuste Componentes 2025**
+
+## **Enlace Web Revision**
+
+[https://componentes-ed.github.io/DSIA_CF4/](https://componentes-ed.github.io/DSIA_CF4/)
+
+#
+
---
+++ vue.config.js
@@ -0,0 +1,20 @@
+const titulo =
+  'Desarrollo de software: programación, arquitectura y herramientas'
+
+module.exports = {
+  publicPath: '',
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/css/_variables.sass"`,
+      },
+    },
+  },
+  chainWebpack: config => {
+    // Mantener la configuración existente del título
+    config.plugin('html').tap(args => {
+      args[0].title = titulo
+      return args
+    })
+  },
+}
